0da6fd7920f20ac37ddd794c216c83ac6396fea8
vijaykumarrpai <vijaykumarrpai@gmail.com>
2019-08-13 20:31:40 +0530

    final commit

---
+++ 1st Sem/PP/Programs/pro.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ int main()
+ {
+     int i = 0;
+     switch (i)
+     {
+         case '0': printf("PRO");
+                 break;
+         case '1': printf("coder");
+                 break;
+         default: printf("PROcoder");
+     }
+     return 0;
+ } 
