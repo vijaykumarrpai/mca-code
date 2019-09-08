cae81bfaaf65edc6ff570c3d2825987b1a3b0aa1
vijaykumarrpai <vijaykumarrpai@gmail.com>
2019-09-08 19:54:38 +0530

    Intro to Android

---
+++ 5thSem/Android/app/src/main/java/com/example/postman/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.postman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
